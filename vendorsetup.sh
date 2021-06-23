7eb411e03f6b23c74d614edfd74663eb7ee0f0f8
pkm774 <mprabhat774@gmail.com>
2021-06-23 22:26:25 +0530

    X00TD: vendorsetup: Use wlan from caf sdm660
    * Use default bt stack
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b lineage-17.1 vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/pkm774/vendor_qcom_opensource_data-ipa-cfg-mgr -b data.lnx.6.0.r12-rel vendor/qcom/opensource/data-ipa-cfg-mgr
 rm -rf hardware/qcom-caf/wlan
-git clone https://android.googlesource.com/platform/hardware/qcom/wlan -b android-11.0.0_r4 hardware/qcom-caf/wlan
+git clone https://source.codeaurora.org/quic/la/platform/hardware/qcom/wlan -b wlan-aosp.lnx.6.0.r10-rel hardware/qcom-caf/wlan
+
 rm -rf vendor/qcom/opensource/power
 git clone https://github.com/pkm774/vendor_qcom_opensource_power -b power.legacyopensource.lnx.1.0.r15-rel vendor/qcom/opensource/power
-
 
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/pkm774/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
@@ -18,11 +18,6 @@ git clone https://github.com/LineageOS/android_hardware_qcom_thermal -b lineage-
 
 rm -rf hardware/libhardware
 git clone https://github.com/LineageOS/android_hardware_libhardware -b lineage-18.0 hardware/libhardware
-
-rm -rf packages/apps/Bluetooth
-git clone https://github.com/Octavi-OS/platform_packages_apps_Bluetooth packages/apps/Bluetooth
-rm -rf vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
-git clone https://github.com/Octavi-OS/platform_vendor_qcom_opensource_commonsys_packages_apps_Bluetooth.git vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 
 rm -rf vendor/sounds
 git clone https://github.com/pkm774/sounds vendor/sounds
