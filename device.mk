da0ba893029a27a31e83948613f1a7d1631994d3
pkm774 <mprabhat774@gmail.com>
2021-06-26 09:01:17 +0530

    X00TD: Build libavservices_minijail from source
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -302,7 +302,9 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libavservices_minijail \
+    libavservices_minijail.vendor
 
 # GPS Config
 PRODUCT_COPY_FILES += \
